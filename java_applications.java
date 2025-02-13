e72cf495a1275ab99dfb9038aa60a373a9ee2f4f
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2025-02-13 17:42:28 +0300

    Update java_applications.java

--- java_applications.java
+++ java_applications.java
@@ -1,3 +1,4 @@
+//banka uygulaması
 public class without_main {
 	
 	public static void main(String[] args) {
@@ -72,4 +73,112 @@ public class without_main {
 		int son = bakiye+ yatırılan_para;
 		return son;
 	}	
+}
+
+
+
+
+
+//giriş ekranı
+class kisi{
+	String[] isim= new String[6];
+	String[] şifre= new String[6];
+	int index=0;
+}
+
+public class without_main {
+	static kisi kisi1 = new kisi();
+	public static void main(String[] args) {
+		calıstır();
+	}
+	public static void calıstır() {
+		boolean op=true;
+		while (op) {
+			menu();
+			Scanner alinan = new Scanner(System.in);
+			String key= alinan.nextLine();
+			switch (key) {
+			case "1":
+				uye_ol();
+				System.out.println(" ");
+				break;
+			case "2":
+				giris_yap();
+				System.out.println(" ");
+				op = false;
+				break;
+			case "q":
+				System.out.println("Esenlikler dileriz:");
+				op=false;
+				break;
+
+			default:
+				System.out.println("hatalı giriş yaptınız tekrar deneyiniz lütfen");
+				break;
+			}	
+		}
+	}
+	public static void menu() {
+		System.out.println("üye ol-->1\n");
+		System.out.println("\n");
+		System.out.println("giriş yap-->2\n");
+		System.out.println("çıkmak için-->'q'\n");
+		
+		
+	}
+	public static void uye_ol() {
+		Scanner alinan = new Scanner(System.in);
+		System.out.println("isminizi giriniz\n");
+		String isim = alinan.nextLine();
+		System.out.println("şifrenizi giriniz\n");
+		String sifre = alinan.nextLine();
+		kisi1.isim[kisi1.index]= isim;
+		kisi1.şifre[kisi1.index]=sifre;
+		kisi1.index++;
+		
+	}
+	public static void giris_yap() {
+		boolean d_y=true;
+		Scanner alinan = new Scanner(System.in);
+		while (d_y) {
+			System.out.println("isminizi giriniz\n");
+			String isim = alinan.nextLine();
+			int i=0;
+			int kullaniciIndex = -1;
+			for (; i <6; i++) {
+				if (isim.equals(kisi1.isim[i]) ) {
+					kullaniciIndex= i;
+					break;
+				}
+			}
+			if (kullaniciIndex==-1) {
+				System.out.println("böyle bir kullanıcı bulunmadı");
+				System.out.println("lütfen üye ol kısmından üye olunuz");
+				calıstır();
+				return;
+			}
+			System.out.println("şifrenizi giriniz\n");
+			Boolean d_y2=true;
+			for (int j = 3; j > 0; j--) {
+				String sifre = alinan.nextLine();
+				if (sifre.equals(kisi1.şifre[kullaniciIndex])) {
+					System.out.println("hoş geldiniz");
+					d_y = false;
+					d_y2=true;
+					break;
+				}
+				else {
+					System.out.printf("hatalı giriş yaptınız kalan hakkınız %d:",j-1);
+					d_y2 = false;
+				}
+			}
+			if (d_y2==false) {
+				System.out.println("\ndeneme hakkınız bitmiştir");
+				d_y =false;
+				return;
+			}
+		}
+
+	}
+
 }
