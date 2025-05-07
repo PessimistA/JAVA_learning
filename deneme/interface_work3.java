477acd63f541377f3dd8aa3fc6a3009c28568caf
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2025-05-07 22:25:27 +0300

    Update interface_work3.java

--- deneme/interface_work3.java
+++ deneme/interface_work3.java
@@ -14,6 +14,11 @@ interface a3{
 interface a4{
 	abstract void printa4();
 }
+interface a5 extends a3{
+	abstract void printa5();
+	abstract void printa6();
+	abstract void printa7();
+}
 
 class b1 implements a1,a2,a3{
 	public b1() {
@@ -73,6 +78,44 @@ class b3 implements a4{
 		System.out.println("a4");
 	}
 }
+class b4 implements a5{
+	public b4() {
+		// TODO Auto-generated constructor stub
+		System.out.println("b4 ün içi");
+		printa1();
+		printa3();
+		printa5();
+		printa6();
+		printa7();
+	}
+
+	@Override
+	public void printa1() {
+		// TODO Auto-generated method stub
+		System.out.println("a1");
+	}
+	@Override
+	public void printa3() {
+		// TODO Auto-generated method stub
+		System.out.println("a3");
+	}
+	@Override
+	public void printa6() {
+		// TODO Auto-generated method stub
+		System.out.println("a6");
+	}
+	@Override
+	public void printa5() {
+		// TODO Auto-generated method stub
+		System.out.println("a5");
+	}
+	@Override
+	public void printa7() {
+		// TODO Auto-generated method stub
+		System.out.println("a7");
+	}
+	
+}
 public class interface_work3 {
 	public static void main(String[] args) {
 		b1 myB1 = new b1();
@@ -82,6 +125,8 @@ public class interface_work3 {
 		a1 myB4 = new b1();
 		a2 myB5 = new b2();
 		a4 myB6 = new b3();
+		
+		b4 myB42 = new b4();
 
 	}
 }
